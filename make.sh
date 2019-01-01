b552346a8b163ddbb76cf6cbbe415b1c9b6b3de1
Ivan Udovin <wilcot@ya.ru>
2019-01-01 13:49:17 +0300

    Remove docker build

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker build -t wilcotcpp-make \
-	--build-arg USER_ID=$(id -u) \
-	--build-arg GROUP_ID=$(id -g) \
-	--quiet docker >/dev/null
-
-docker run --rm --privileged -v "$(realpath .)":/app wilcotcpp-make $1
